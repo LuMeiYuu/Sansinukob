0a0cb0159a75588ccd894df31d7b5893a09c1bc9
LuMeiYuu <mayaggabao28@gmail.com>
2025-08-30 14:29:30 +0800

    created flowerData

---
+++ flowerData.js
@@ -0,0 +1,19 @@
+
+
+var flowerData = [
+  {
+    name: "Rose",
+    growthTime: 30,
+    image: "icons/rose.png"
+  },
+  {
+    name: "Tulip",
+    growthTime: 25,
+    image: "icons/tulip.png"
+  },
+  {
+    name: "Sunflower",
+    growthTime: 35,
+    image: "icons/sunflower.png"
+  }
+];
